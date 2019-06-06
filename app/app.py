b979326e4456d87900e6abf0b6f05e056c3e1c69
David Bradway <dpb6@duke.edu>
2019-06-06 14:57:33 -0400

    add signature validation

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 #!/usr/local/bin/python3
 import os
+import utils.validate_slack_signature
 from flask import Flask, request
 from redis import Redis
 
 # This `app` represents your existing Flask app
 app = Flask(__name__)
+SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, endpoint="/slack/events", app)
+
 r = Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], decode_responses=True)
 bind_port = int(os.environ['BIND_PORT'])
 
@@ -67,6 +71,7 @@ def handle(command):
 
 
 @app.route('/remember', methods=['POST'])
+@utils.validate_slack_signature
 def remember():
     """Flask route for remmeber slash command. Prepend user_id~ to text entered and pass on to handler"""
     return handle(request.form['user_id'] + '~' + request.form['text'])
