cbfc501f70f3d7b70ab8528854cdf8271c52e18a
David Bradway <dpb6@duke.edu>
2019-06-06 15:19:36 -0400

    move decorator into app

--- app/app.py
+++ app/app.py
@@ -1,16 +1,60 @@
 #!/usr/local/bin/python3
 import os
-import utils.validate_slack_signature
+import datetime
+import hashlib
+import hmac
 from flask import Flask, request
 from redis import Redis
 
 # This `app` represents your existing Flask app
 app = Flask(__name__)
 SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
-slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, endpoint="/slack/events", app)
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
 
 r = Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], decode_responses=True)
 bind_port = int(os.environ['BIND_PORT'])
+
+
+def validate_slack_signature(func):
+    """
+    Cryptographically validates that the incoming message came from Slack, using the
+    Slack 'signing secret'
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        # Retrieve the X-Slack-Request-Timestamp header on the HTTP request
+        timestamp = request.headers.get("X-Slack-Request-Timestamp")
+
+        if absolute_value(datetime.datetime.now() - timestamp) > 60 * 5: 
+            # The request timestamp is more than five minutes from local time.
+            # It could be a replay attack, so let's ignore it.
+            return "", http.HTTPStatus.NO_CONTENT
+
+        # Retrieve the X-Slack-Signature header on the HTTP request, and the body of the request
+        signature = request.headers.get("X-Slack-Signature")
+        body = request.get_data(as_text=True)
+
+        # Concatenate the version number (right now always v0), 
+        #  the timestamp, and the body of the request.
+        # Use a colon as the delimiter and encode as bytestring
+        format_req = str.encode(f"v0:{timestamp}:{body}")
+
+        # Encode as bytestring
+        encoded_secret = str.encode(SLACK_SIGNING_SECRET)
+
+        # Using HMAC SHA256, hash the above basestring, using the Slack Signing Secret as the key.
+        request_hash = hmac.new(encoded_secret, format_req, hashlib.sha256).hexdigest()
+
+        # Compare this computed signature to the X-Slack-Signature header on the request.
+        if hmac.compare_digest(f"v0={request_hash}", signature):
+            # hooray, the request came from Slack! Run the decorated function
+            return func(*args, **kwargs)
+        else:
+            return "", http.HTTPStatus.NO_CONTENT
+
+    return wrapper
 
 
 def splitkey(userkey):
@@ -54,27 +98,27 @@ def get(name=None):
             return mystr
 
 
-def handle(command):
-    """Handle a command that has been passed from the Flask route. Either help, set, get, or delete."""
-    if 'help' in command:
-        return 'HELP. Usage (set, get, del): \n /remember key=value \n /remember key \n /remember key='
-    else:
-        if '=' in command: # pass to set
-            mylist = command.split('=')
-            name, value = mylist[0].strip(), mylist[1].strip()
-            if value is '':
-                return set(name) # delete
-            else:
-                return set(name,value) # set
-        else: # pass to get
-            return get(command)
-
-
 @app.route('/remember', methods=['POST'])
-@utils.validate_slack_signature
+@validate_slack_signature
 def remember():
     """Flask route for remmeber slash command. Prepend user_id~ to text entered and pass on to handler"""
-    return handle(request.form['user_id'] + '~' + request.form['text'])
+    text = request.form['text']
+
+    """Handle a command with either help, set, get, or delete."""
+    if 'help' in text:
+        return 'HELP. Usage (set, get, del): \n /remember key=value \n /remember key \n /remember key='
+    else:
+        user_id = request.form['user_id']
+        if '=' in text:
+            mylist = text.split('=')
+            name, value = mylist[0].strip(), mylist[1].strip()
+            if value is '':
+                return set(user_id + '~' + name) # delete
+            else:
+                return set(user_id + '~' + name,value) # set
+        else:
+            return get(user_id + '~' + text) # get
+    return 
 
 
 if __name__ == "__main__":
